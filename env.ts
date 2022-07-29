22df868702ce5e345e280112a6f6a154a4949577
Lisandro Fernández <lifofernandez@gmail.com>
2022-07-29 14:57:20 -0300

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
-  SITE_NAME: 'My SaaS Startup',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
-  URL: 'https://next-saas-starter-ashy.vercel.app/',
+  SITE_NAME: 'Mantis',
+  OG_IMAGES_URL: 'mantis.vercel.app/og-images/',
+  URL: 'https://mantis.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
